8f52efc1ed8e230e643cf3cab965170bc0bde92e
babailanxx <babailanxx@gmail.com>
2022-09-30 05:49:40 +0800

    adding animation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop();
     var pageBottom = pageTop + $(window).height();
-    var tags = $(".tag");
+    var tags = $(".fade-left");
 
     for (var i = 0; i < tags.length; i++) {
         var tag = tags[i];
@@ -13,3 +13,35 @@ $(document).on("scroll", function () {
         }
     }
 });
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".fade-right");
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("fade-in-right");
+        } else {
+            $(tag).removeClass("fade-in-right");
+        }
+    }
+});
+
+// $(document).ready(function () {
+//     $("a").click(function () {
+//         closeNav();
+//     });
+// });
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    console.log('clicked');
+}
+
