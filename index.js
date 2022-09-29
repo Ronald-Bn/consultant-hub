e68ead61d2509d13a6ac14ee133e1cb2ed554d05
babailanxx <babailanxx@gmail.com>
2022-09-29 23:59:17 +0800

    fixing font size, padding on different devices

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("fade-in-left");
+        } else {
+            $(tag).removeClass("fade-in-left");
+        }
+    }
+});
